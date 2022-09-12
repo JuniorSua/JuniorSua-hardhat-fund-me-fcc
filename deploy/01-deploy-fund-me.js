338f50041b8f03d897cbf9e8f9f042c3c01dc40c
Junior Suarez-Leyva <juniorsuarez-leyva@MacBook-Pro.local>
2022-09-12 03:37:16 -0400

    first commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,64 @@
+// import
+// main function
+// calling main function
+
+const { network } = require("hardhat")
+
+// async function deployFunc() {
+//     console.log("Hi!")
+// }
+// module.exports.default = deployFunc
+
+// const helperConfig = require("../helper-......")
+// const networkConfig = helperConfig.networkConfig
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    //const { getNamedAccounts, deployments } = hre
+    // hre.getNamedAccounts
+    // hre.deployments
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // if chainId is X use address Y
+    // if chainId is Z use address A
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        // console.log(
+        //     `This is the Price Address ----- ${ethUsdPriceFeedAddress} ------`
+        // )
+        // console.log(`This is the chainId ----- ${chainId} ------`)
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    // if the contract doesn't exist, we deploy a minimal version of
+    // for out local testing
+
+    // well what happens when we want to change chains?
+    // when going for localhost or hardhat network we want to use a mock
+    log("---------------------------------------------------------")
+    log("Deploying FundMe and waiting for confirmation...")
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args, // put price feed address
+        log: true,
+        waitConfirmations: network.config.blockConfirmation || 1,
+    })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("-------------------------------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
